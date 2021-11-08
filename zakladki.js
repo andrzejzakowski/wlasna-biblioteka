b7595f2e858f88771d1a9423fa363f54f5ab07e4
andrzejzakowski <diblox31011@gmail.com>
2021-11-08 18:03:39 +0100

    karta 8 i 9

--- zakladki.js
+++ zakladki.js
@@ -41,7 +41,7 @@
                 //------------------------------------------------------------------------------------------------------------------------
                 //Skrypt karta numer 7
                 //------------------------------------------------------------------------------------------------------------
-                //Skrypt do karty numer 6
+                //Skrypt do karty numer 7
                 var zakladka_1_karta_7 = document.querySelector(".zakladka_1_karta_7");
                 var zakladka_2_karta_7 = document.querySelector(".zakladka_2_karta_7");
                 var zakladka_3_karta_7 = document.querySelector(".zakladka_3_karta_7");
@@ -54,17 +54,17 @@
 
 
                 zakladka_1_karta_7.addEventListener("click", function(){
-                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 6.1</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 7.1</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
                     updateActiveState(zakladka_1_karta_7);
                 })
 
                 zakladka_2_karta_7.addEventListener("click", function(){
-                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 6.2</h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 7.2</h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
                     updateActiveState(zakladka_2_karta_7);
                 })
 
                 zakladka_3_karta_7.addEventListener("click", function(){
-                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 6.3</h3>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+                    Content_zakladka_karta_7.innerHTML="<h3 style='text-align: center;'>Karta 7.3</h3>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
                     updateActiveState(zakladka_3_karta_7);
                 })
 
@@ -75,6 +75,52 @@
                         }
                         else{
                             zakladka_karta_7.classList.add("active");
+                        }
+                    })
+                }
+
+
+
+
+
+
+                //------------------------------------------------------------------------------------------------------------------------
+                //Skrypt karta numer 8
+                //------------------------------------------------------------------------------------------------------------
+                //Skrypt do karty numer 8
+                var zakladka_1_karta_8 = document.querySelector(".zakladka_1_karta_8");
+                var zakladka_2_karta_8 = document.querySelector(".zakladka_2_karta_8");
+                var zakladka_3_karta_8 = document.querySelector(".zakladka_3_karta_8");
+
+                var wszystkie_zakladki_karta_8 = document.querySelectorAll(".tab_karta_8");
+
+                var Content_zakladka_karta_8 = document.querySelector(".tab-content_karta_8");
+
+                var choosenTab;
+
+
+                zakladka_1_karta_8.addEventListener("click", function(){
+                    Content_zakladka_karta_8.innerHTML="<h3 style='text-align: center;'>Karta 8.1</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+                    updateActiveState(zakladka_1_karta_8);
+                })
+
+                zakladka_2_karta_8.addEventListener("click", function(){
+                    Content_zakladka_karta_8.innerHTML="<h3 style='text-align: center;'>Karta 8.2</h3>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+                    updateActiveState(zakladka_2_karta_8);
+                })
+
+                zakladka_3_karta_8.addEventListener("click", function(){
+                    Content_zakladka_karta_8.innerHTML="<h3 style='text-align: center;'>Karta 8.3</h3>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+                    updateActiveState(zakladka_3_karta_8);
+                })
+
+                function updateActiveState(activeTab_karta_8){
+                    wszystkie_zakladki_karta_8.forEach(function(zakladka_karta_8){
+                        if(zakladka_karta_8 != activeTab_karta_8){
+                            zakladka_karta_8.classList.remove("active");
+                        }
+                        else{
+                            zakladka_karta_8.classList.add("active");
                         }
                     })
                 }
